7bfae3076842777f5d392c903bab651ed2acc04b
Kashigu <78795585+Kashigu@users.noreply.github.com>
2024-08-29 22:25:52 +0100

    Animes components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 import '../globals.css'; // Ensure this path is correct
 import MainImage from '../components/MainImage'; // Ensure this path is correct
+import Animes from '@/components/Animes';
+
 
 export default function Home() {
   return (
-    <div className="bg-custom-blue-dark">
+    <>
+    
+    <div className="bg-custom-blue-dark ">
       <MainImage />
       {/* Header is included in the layout */}
     </div>
+    <div className=" bg-custom-blue-dark">
+      <Animes/>
+    </div>
+    </>
   );
 }
---
+++ src/components/Animes.tsx
@@ -0,0 +1,57 @@
+import codegeassImage from "../../public/images/codegeassF.jpg"; 
+import Image from 'next/image';
+
+function Animes() {
+    return (
+        <>
+        <div className="flex flex-col mb-12 w-full ">
+            
+        </div>
+        <div className="container mx-auto">{/* Centering the entire content */}
+            <div className="flex flex-col mb-12 w-full text-white text-4xl   font-bold mt-4 justify-start">
+                Latest Releases
+            </div>
+                <div className="grid grid-cols-3 gap-x-8 gap-y-4 bg-custom-blue-dark "> {/* px-4 adds padding to prevent images from touching the screen edges */}
+                    <div className="flex flex-col items-center">
+                        <Image
+                            src={codegeassImage} 
+                            alt="Code Geass"
+                            style={{ objectFit: "cover" }} 
+                            priority={false}
+                        />
+                        <h2 className="text-white text-xl font-bold mt-4">Code Geass</h2>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <Image
+                            src={codegeassImage} 
+                            alt="Code Geass"
+                            style={{ objectFit: "cover" }} 
+                            priority={false}
+                        />
+                        <h2 className="text-white text-xl font-bold mt-4">Code Geass</h2>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <Image
+                            src={codegeassImage} 
+                            alt="Code Geass"
+                            style={{ objectFit: "cover" }} 
+                            priority={false}
+                        />
+                        <h2 className="text-white text-xl font-bold mt-4">Code Geass</h2>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <Image
+                            src={codegeassImage} 
+                            alt="Code Geass"
+                            style={{ objectFit: "cover" }} 
+                            priority={false}
+                        />
+                        <h2 className="text-white text-xl font-bold mt-4">Code Geass</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Animes;
